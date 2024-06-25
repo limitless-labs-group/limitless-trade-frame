db99931c6d01c3608862af45c71600b4e3867e89
atarapata <oleksandr@limitless.network>
2024-06-25 15:38:36 +0100

    changed functionality

--- queries/constants.ts
+++ queries/constants.ts
@@ -1,3 +1,3 @@
 import {base, baseSepolia} from "viem/chains";
 
-export const defaultChain = process.env.NEXT_PUBLIC_CHAIN_TYPE === 'testnet' ? baseSepolia : base
+export const defaultChain = process.env.CHAIN_TYPE === 'testnet' ? baseSepolia : base
