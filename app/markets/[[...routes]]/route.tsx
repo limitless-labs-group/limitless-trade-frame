4d5851a0bde7f0c533d9efe961b288c51956e4da
atarapata <oleksandr@limitless.network>
2024-06-25 14:23:16 +0100

    changed functionality

--- app/markets/[[...routes]]/route.tsx
+++ app/markets/[[...routes]]/route.tsx
@@ -25,7 +25,8 @@ const app = new Frog<{State: State}>({
     }
 })
 
-const accountToInvestmentAmount: Record<string, bigint> = {};
+let accountToInvestmentAmountRaw = '0'
+let accountToInvestmentAmountBI: bigint
 
 app.frame('/:address', async (c) => {
     const { deriveState } = c
@@ -105,7 +106,8 @@ app.transaction("/approve-tx/:address/:decimals/:collateralAddress", (c) => {
     }
     const decimals = +c.req.param('decimals')
     const investmentAmount = parseUnits(inputText || '1', decimals);
-    accountToInvestmentAmount[address] = investmentAmount;
+    accountToInvestmentAmountRaw = inputText || '1';
+    accountToInvestmentAmountBI = investmentAmount
     const collateralTokenAddress = c.req.param('collateralAddress')
     const marketAddress = c.req.param('address')
     return c.contract({
@@ -138,10 +140,27 @@ app.frame('/buy/:address', async (c) => {
 
     const collateralToken = tokensResponse.find((token) => token.address.toLowerCase() === marketResponse.collateralToken[defaultChain.id].toLowerCase()) as Token
 
-    const getImageDynamicContent = async () => {
-        if(inputText && buttonValue) {
-            const values = await getQuote(marketResponse, inputText, collateralToken, buttonValue === 'buyYes' ? 0: 1, marketResponse.prices)
-            return (
+    const values = await getQuote(marketResponse, accountToInvestmentAmountRaw, collateralToken, buttonValue === 'buyYes' ? 0: 1, marketResponse.prices)
+
+    return c.res({
+        image: (
+            <div style={{
+                color: 'white',
+                display: 'flex',
+                flexDirection: 'column',
+                fontSize: 60,
+                backgroundImage: `url("https://storage.googleapis.com/limitless-exchange-assets/assets/background.png")`,
+                backgroundSize: '100% 100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100%',
+                paddingLeft: '15%',
+                paddingRight: '15%',
+                maxWidth: '100%'
+            }}>
+                <img src="/logo.png" alt="logo" style={{width: '185px', height: '40px'}}/>
+                <span style={{fontSize: '32px', fontWeight: 'bold', marginTop: '20px'}}>Have skin in your beliefs</span>
+                <span style={{fontSize: '40px', fontWeight: 'bold', marginTop: '60px', textAlign: 'center'}}>{marketResponse.title}</span>
                 <>
                     <div style={{display: 'flex', gap: '100px', marginTop: '40px'}}>
                         <div style={{display: 'flex', flexDirection: 'column'}}>
@@ -173,53 +192,6 @@ app.frame('/buy/:address', async (c) => {
                         </div>
                     </div>
                 </>
-            )
-        }
-        return (
-            <div style={{display: 'flex', gap: '100px', marginTop: '40px'}}>
-                <div
-                    style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <span style={{color: '#71FF65', fontSize: '28px'}}>{marketResponse.prices[0].toFixed(2)}%</span>
-                    <span style={{color: '#747675', fontSize: '28px'}}>Chance</span>
-                </div>
-                <div
-                    style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <span style={{
-                        color: '#71FF65',
-                        fontSize: '28px'
-                    }}>{formatUnits(marketResponse.liquidity, collateralToken.decimals)} {collateralToken.symbol}</span>
-                    <span style={{color: '#747675', fontSize: '28px'}}>Volume</span>
-                </div>
-                <div
-                    style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <span style={{color: '#71FF65', fontSize: '28px'}}>{marketResponse.expirationDate}</span>
-                    <span style={{color: '#747675', fontSize: '28px'}}>Deadline</span>
-                </div>
-            </div>
-        )
-    }
-
-
-    return c.res({
-        image: (
-            <div style={{
-                color: 'white',
-                display: 'flex',
-                flexDirection: 'column',
-                fontSize: 60,
-                backgroundImage: `url("https://storage.googleapis.com/limitless-exchange-assets/assets/background.png")`,
-                backgroundSize: '100% 100%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '100%',
-                paddingLeft: '15%',
-                paddingRight: '15%',
-                maxWidth: '100%'
-            }}>
-                <img src="/logo.png" alt="logo" style={{width: '185px', height: '40px'}}/>
-                <span style={{fontSize: '32px', fontWeight: 'bold', marginTop: '20px'}}>Have skin in your beliefs</span>
-                <span style={{fontSize: '40px', fontWeight: 'bold', marginTop: '60px', textAlign: 'center'}}>{marketResponse.title}</span>
-                {await getImageDynamicContent()}
             </div>
         ),
         intents: [
@@ -239,13 +211,13 @@ app.transaction('/:collateralContract/buy/:index', async (c) => {
         address: previousState.marketAddress as Address,
         abi: fixedProductMarketMakerABI,
         functionName: "calcBuyAmount",
-        args: [accountToInvestmentAmount[frameData?.address || '0x'], outcomeIndex],
+        args: [accountToInvestmentAmountBI, outcomeIndex],
     });
 
     return c.contract({
         abi: fixedProductMarketMakerABI,
         functionName: "buy",
-        args: [accountToInvestmentAmount[frameData?.address || '0x'], outcomeIndex, minOutcomeTokensToBuy],
+        args: [accountToInvestmentAmountBI, outcomeIndex, minOutcomeTokensToBuy],
         chainId: "eip155:84532",
         to: previousState.marketAddress as Address,
     });
